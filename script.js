ee0ecfbf6bb050be3e6da8667e99205284c90adf
tylering6 <tylering6@gmail.com>
2024-07-02 01:42:04 -0400

    updated

--- script.js
+++ script.js
@@ -7,40 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 function submitContactForm() {
-    const number = document.getElementById('number').value;
-    const school = document.getElementById('school').value;
-    const contact = document.getElementById('contact').value;
-    const stakes = document.getElementById('stakes').value;
-    const elevate = document.getElementById('elevate').value;
-    const high = document.getElementById('high').value;
-    const record = document.getElementById('record').value;
-    const info = document.getElementById('info').value;
+    const contactForm = document.getElementById('contact-form');
+    const formData = new FormData(contactForm);
 
-    const formData = {
-        Number: number,
-        School: school,
-        Contact: contact,
-        Stakes: stakes,
-        Elevate: elevate,
-        High: high,
-        Record: record,
-        Info: info
-    };
-
-    fetch('https://script.google.com/macros/s/AKfycbyHyJGLa5L2jNpNEGlKCgFX-zSuzSvVuXY1hGL49HH5ivBf6ApUMb_dh1O668su1pBv/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxy7jFNhHOef3ie83rvkFq6_ipk1FlBYg-3KxvvwGjSYMj1YFngDhogcU59sDQ2Zd-x/exec', {
         method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+        body: formData
     })
-    .then(response => {
-        console.log('Form submitted successfully');
-        alert('Thank you for contacting us!');
-        contactForm.reset();
+    .then(response => response.json())
+    .then(data => {
+        console.log('Form submitted successfully', data);
+        window.location.href = 'Thank-You.html';
     })
     .catch(error => {
         console.error('Error submitting form:', error);
